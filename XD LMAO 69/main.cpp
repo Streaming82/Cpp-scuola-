a2faa100adf8f00f96349b1af577492b428c4b2c
Streaming82 <79966645+Streaming82@users.noreply.github.com>
2021-03-03 09:10:55 +0100

    Add files via upload

---
+++ XD LMAO 69/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int lmao;
+	
+cout<<"ciao mamma sono su youtube :";
+	cin>>lmao;
+if (lmao==69) 
+	cout<<"XD Lmao 69";
+else 
+	cout<<"noob";
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
